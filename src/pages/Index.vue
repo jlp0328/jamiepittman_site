6c025abbb939e63a97a76c57436f1ee59369aa11
jlp0328 <jlp0328@gmail.com>
2019-08-13 21:39:57 -0400

    Update message on page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,10 +4,10 @@
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
     
-    <h1>Hello, world!</h1>
+    <h1>Jamie Pittman's Site</h1>
    
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+      My personal site is currently under construction... brought to you by Gridsome!
     </p>
 
     <p class="home-links">
